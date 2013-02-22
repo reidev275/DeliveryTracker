35c71bb6fda4788c797602d16bd55a132f1af6e7
Reid Evans <reidev275@gmail.com>
2013-02-21 19:20:20 -0500

    create memoryusersrepository

---
+++ DeliveryTracker/Repositories/MemoryUsersRepository.cs
@@ -0,0 +1,33 @@
+﻿using DeliveryTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeliveryTracker.Repositories
+{
+    public class MemoryUsersRepository : IUsersRepository
+    {
+        private static readonly List<User> _users = new List<User>();
+
+        public User GetByUserName(string userName)
+        {
+            if (userName == null) throw new ArgumentNullException("userName");
+            return _users.FirstOrDefault(x => x.Id == userName);
+        }
+
+        public void Update(User user)
+        {
+            if (user == null) throw new ArgumentNullException("user");
+            var tobeUpdated = GetByUserName(user.Id);
+            tobeUpdated.HintAnswer = user.HintAnswer;
+            tobeUpdated.HintQuestion = user.HintQuestion;
+            tobeUpdated.Password = user.Password;
+        }
+
+        public void Create(User user)
+        {
+            if (user == null) throw new ArgumentNullException("user");
+            _users.Add(user);
+        }
+    }
+}
