5fefefc79567a6c29eb3df367f6b5f7e628fdb3f
Reid Evans <reidev275@gmail.com>
2013-07-20 08:45:55 -0400

    add touch events

--- DeliveryTracker/Scripts/canvas.js
+++ DeliveryTracker/Scripts/canvas.js
@@ -1,7 +1,12 @@
 ﻿(function ($) {
-    $(document).delegate("canvas", "touchmove, touchstart, touchend", function (e) {
+    var getCoordinates = function(e, obj) {
         e.preventDefault();
-    });
+        var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0],
+            elm = $(obj).offset(),
+            x = touch.pageX - elm.left,
+            y = touch.pageY - elm.top;
+        return { X: x, Y: y };
+    };
 
     $.fn.Expand = function () {
         if (this[0] && this[0].getContext) {
@@ -15,7 +20,6 @@
     };
 
     $.fn.PenTool = function () {
-
         if (this[0] && this[0].getContext) {
             var canvas = this[0],
                 context = canvas.getContext("2d"),
@@ -28,10 +32,28 @@
                 isMouseDown = false;
             }).mousemove(function (e) {
                 if (isMouseDown) {
-                    context.lineTo(e.offsetX, e.offsetY);
-                    context.stroke();
+                    drawTo(e.offsetX, e.offsetY);
                 }
             });
+
+            $(document).delegate(this, 'touchmove', function (e) {
+                if (isMouseDown) {
+                    var coords = getCoordinates(e, this);
+                    drawTo(coords.X, coords.Y);
+                }
+            }).delegate(this, 'touchstart', function (e) {
+                isMouseDown = true;
+                var coords = getCoordinates(e, this);
+                context.moveTo(coords.X, coords.Y);
+            }).delegate(this, 'touchend', function (e) {
+                isMouseDown = false;
+            });
+
+            var drawTo = function (x, y) {
+                context.lineTo(x, y);
+                context.stroke();
+            };
+
 
             this.clearPen = function () {
                 context.clearRect(0, 0, canvas.width, canvas.height);
@@ -61,6 +83,20 @@
                 if (isMouseDown) {
                     points.push({ X: e.offsetX, Y: e.offsetY });
                 }
+            });
+
+            $(document).delegate(this, 'touchmove', function (e) {
+                if (isMouseDown) {
+                    var coords = getCoordinates(e, this);
+                    points.push({ X: coords.X, Y: coords.Y });
+                }
+            }).delegate(this, 'touchstart', function (e) {
+                isMouseDown = true;
+                var coords = getCoordinates(e, this);
+                points.push({ X: coords.X, Y: coords.Y });
+            }).delegate(this, 'touchend', function (e) {
+                isMouseDown = false;
+                penups.push(points.length);
             });
 
             this.clearTopaz = function () {
