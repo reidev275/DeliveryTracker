f5507d16872221a8ff225c2ae3be95c11f4e4c34
Reid Evans <reidev275@gmail.com>
2013-08-07 12:57:32 -0400

    add responsive input validation

---
+++ DeliveryTracker/Scripts/ko.validate.js
@@ -0,0 +1,13 @@
+﻿(function (ko, $) {
+	var run = function (element, valueAccessor) {
+		var value = valueAccessor();
+		if (value === '') value == null;
+		if (value) $(element).addClass('has-success').removeClass('has-error');
+		else $(element).addClass('has-error').removeClass('has-success');
+	};
+
+	ko.bindingHandlers.validate = {
+		init: run,
+		update: run
+	};
+}(ko, jQuery));
